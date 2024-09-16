97ca3a2ec03dcc64b8817baefd33dd85a448e8cd
Georgy Voskanyan <hello@voskanyan.dev>
2024-09-16 13:11:39 +0200

    FEAT: Add printInfo method to the Instrument base class

--- src/Instrument.java
+++ src/Instrument.java
@@ -1,11 +1,15 @@
 public abstract class Instrument {
     abstract public void play();
-    private String name;
-    private double price;
+    private final String name;
+    private final double price;
 
     public Instrument(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public void printInfo() {
+        System.out.println("L'instrument " + name + " te el preu de " + price + ".");
     }
 }
 
